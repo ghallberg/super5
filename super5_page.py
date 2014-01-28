62ab0630aa8d417791762c4ef35b62a83a416232
Gustaf Hallberg <ghallberg@gmail.com>
2014-01-28 21:25:06 +0100

    Added some styling.

--- super5_page.py
+++ super5_page.py
@@ -1,19 +1,21 @@
-from bottle import route, run, template, request
+from bottle import route, run, template, request, static_file
 from super5 import count_address_word
 
-@route('/super5')
-def index():
-    in_address = 'http://www.aftonbladet.se'
-    in_word = 'super'
+def apply_template(in_address, in_word):
     address_count = count_address_word(in_address, in_word)
     return template('super5_template', address=in_address, word=in_word, number=address_count)
+@route('/super5')
+def index():
+    return apply_template('http://www.aftonbladet.se', 'super')
 
 @route('/super5', method='POST')
 def custom():
     in_address = request.forms.get('address')
     in_word = request.forms.get('word')
-    address_count = count_address_word(in_address, in_word)
-    return template('super5_template', address=in_address, word=in_word, number=address_count)
+    return apply_template(in_address, in_word)
 
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./static')
 
-run(host='192.168.110.162', port='8080')
+run(host='0.0.0.0', port='8080', reloader = True)
--- super5_template.tpl
+++ super5_template.tpl
@@ -2,14 +2,19 @@
     <title>
         Number of {{word}}´s on {{address}}
     </title>
+    <LINK href="/static/super5.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    There are {{number}} {{word}}'s on {{address}}
+    <div id = 'output' class = 'centered'>
+        There are <b>{{number}}</b> <b>{{word}}</b>'s on <b>{{address}}</b>
+    </div>
     
 
-    <form action = '/super5' method = 'post'>
-        <label>Address: <input type = 'text' name = 'address'> </label>
-        <label>Word: <input type = 'text' name = 'word'> </label>
-        <input type = 'submit' value = 'COUNT!'>
-    </form>
+    <div id = 'input'>
+        <form action = '/super5' method = 'post' class = 'centered'>
+            <label>Address: <input type = 'text' name = 'address'> </label>
+            <label>Word: <input type = 'text' name = 'word'> </label>
+            <input type = 'submit' value = 'COUNT!'>
+        </form>
+    </div>
 </body>
